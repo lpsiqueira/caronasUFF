c2c1fcc4508b486f599e380417acd8d9a33c608d
Lucas P S <lpsiqueira@id.uff.br>
2019-07-06 21:03:03 -0300

    ajustes

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #iniciando banco de dados
-postgres -D /usr/local/var/postgres #digitar local do executavel postgres
+#postgres -D /usr/local/var/postgres #digitar local do executavel postgres
 #criando usuario e banco de dados
 createuser -dP trabalhobd2
 createdb -U trabalhobd2 AppCaronas
 #criando tabelas
-psql -f *.sql -q AppCaronas trabalhobd2
+files=(*.sql)
+for file in ${files[@]}; do
+    psql -f ${file} -q AppCaronas trabalhobd2
+done
