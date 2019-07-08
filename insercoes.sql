00dda53d51b8d3763e81b7496e0b331d2daa0408
Moutella <joaogabrielmsc@gmail.com>
2019-07-08 01:37:36 -0300

    restriçao 1

---
+++ insercoes.sql
@@ -0,0 +1,12 @@
+INSERT INTO LUGAR VALUES(1, 'São Domingos', NULl);
+INSERT INTO CAMPUS VALUES(1, 'Rua nao sei das quantas', 'Praia Vermelha');
+INSERT INTO LUGAR VALUES(2, 'Praia Vermelha', 1);
+
+INSERT INTO USUARIO VALUES(1, 'Joao', 'das Neves', '123321321', 1, 'emailqualquer@mail.com');
+INSERT INTO MODELO_CARRO VALUES(1, 'CARRAO', 'BMW');
+INSERT INTO CARRO VALUES(1, 'ABC-1234', 1, 1);
+INSERT INTO MOTORISTA VALUES(1,1);
+
+
+
+insert into carona values(1, 1, 1, 1, 1, 1, '23/10/2015', '22:32', 5); //dá erro com o trigger de verificaçao de campus
