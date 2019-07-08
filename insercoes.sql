8e77d01a55190f9cfde700b9a930731c2c9dba55
Moutella <joaogabrielmsc@gmail.com>
2019-07-08 02:29:16 -0300

    funçao de dar nota recente

--- insercoes.sql
+++ insercoes.sql
@@ -10,3 +10,7 @@ INSERT INTO MOTORISTA VALUES(1,1);
 
 
 insert into carona values(1, 1, 1, 1, 1, 1, '23/10/2015', '22:32', 5); //dá erro com o trigger de verificaçao de campus
+insert into carona values(2, 1, 1, 2, 1, 1, '05/07/2019', '22:32', 5);
+INSERT INTO USUARIO values(2, 'Lucas', 'Luis', '321321321', 1, 'outroemail@mail.com');
+insert into reserva(id, usuario_id, carona_id) values(1,2,1);
+INSERT INTO passageiro(id, usuario_id, carona_id) values(1,2,1);
