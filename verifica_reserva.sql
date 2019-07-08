37fd92087be7609b9dabd63a9a8e57e76f5c26a4
Lucas Bicalho <lucassbcosta@hotmail.com>
2019-07-08 02:23:22 -0300

    modificando trigger reserva_de_usuario

--- verifica_reserva.sql
+++ verifica_reserva.sql
@@ -1,8 +1,5 @@
 DROP TRIGGER IF EXISTS reserva_de_usuario ON passageiro;
 CREATE OR REPLACE FUNCTION verifica_reserva() RETURNS TRIGGER AS $$
-DECLARE
-    reserva_usuario_id int;
-    reserva_carona_id int;
 BEGIN
 	IF (EXISTS (SELECT * FROM reserva WHERE reserva.usuario_id=NEW.usuario_id AND reserva.carona_id=NEW.carona_id)) THEN
         RETURN NEW;
